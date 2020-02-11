bc5bda436d8a40d2212897d1ba0c7b5d2e5a0d00
Matthew Grove <32196181+MIGrove@users.noreply.github.com>
2020-02-11 13:41:33 +0200

    Reading board file into 2D array

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,6 +1,37 @@
+import princeton.In;
+import java.io.File;
 
 public class Traversal {
+	private static String filenameBoard, filenameMoves;
+	private static String boardName;
+	private static char[][] board;
+	
 	public static void main(String[] args) {
+		filenameBoard = args[0];
+		filenameMoves = args[1];
 		
+		readBoardFile();
+	}
+	
+	private static void readBoardFile() {
+		In readFileBoard = new In(new File(filenameBoard));
+		
+		boardName = readFileBoard.readLine();
+		int rows = readFileBoard.readInt();
+		int columns = readFileBoard.readInt();
+		
+		board = new char[rows][columns];
+				
+		System.out.printf("\nrows: %d\tcolumns: %d\n\n", rows, columns);
+		
+		for (int r=0; r < rows && readFileBoard.hasNextLine(); r++) {
+			for (int c=0; c < columns && readFileBoard.hasNextChar(); c++) {
+				System.out.printf("reading [%d][%d]\n", r, c);
+				char element = readFileBoard.readChar();
+				board[r][c] = element;
+			}
+			readFileBoard.readLine();
+		}
+		System.out.println("reading complete");
 	}
 }
