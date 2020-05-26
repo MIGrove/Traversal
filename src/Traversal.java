16f9c890105b7e09b97e285abdb5624cad2e1251
Matthew Grove <32196181+MIGrove@users.noreply.github.com>
2020-05-27 00:53:00 +0200

    text version possibly complete
    No errors found in testing as of yet.

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,7 +1,5 @@
-import javax.lang.model.element.NestingKind;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -9,13 +7,15 @@ import java.util.Scanner;
 //	NB: As OOP, or more specifically objects, are not allowed in this project, every method has been made static.
 
 public class Traversal {
+	private static int[] currentPlayerPosition = new int[2];
+	private static int turn = 0, totalMoves;
+	private static int rows, columns;
+	private static boolean gameOver = false;
 	private static String filepathBoard, filepathMoves;
 	private static String boardName;
-	private static String[][] board;
-	private static int[] currentPlayerPosition = new int[2];
 	private static ArrayList<String> placementQueue = new ArrayList<>();
-	private static int turn = 0;
-	private static int rows, columns;
+	private static String[][] board;
+	private static String[] moves;
 	private static String[] moverTypes = new String[] {
 			"u", "d", "l", "r",
 			"U", "D", "L", "R"
@@ -29,66 +29,68 @@ public class Traversal {
 	//	constants
 	private static final int ROW = 0;
 	private static final int COLUMN = 1;
-	
+
+	//	format for calling pieces is as follows:	board[row][column]	(row first!)
+
+	/*
+	moves:
+		'h' --> left move
+		'j' --> down move
+		'k' --> up move
+		'l' --> right move
+		'x' --> quit command
+	 */
+
 	public static void main(String[] args) {
-
-		//	format for calling pieces is as follows:	board[row][column]	(row first!)
-
-		/*
-		moves:
-			'h' --> left move
-			'j' --> down move
-			'k' --> up move
-			'l' --> right move
-			'x' --> quit command
-		 */
-
 		//filepathBoard = args[0];
 		//filepathMoves = args[1];
 		filepathBoard = "samples\\board_test.txt";
 		filepathMoves = "samples\\moves_test.txt";
 
 		//	initialisation
-		readBoard();
+		initialise();
 
 		//	post-init.
-		System.out.println("----------");
-		getMoverMapOnTurn(5);
-		System.out.println("miscMap:");
-		print2DArray(generateMiscMap());
-		System.out.println("----------");
+		while(!gameOver && turn <= totalMoves) {
+			String[][] map0 = generateBlankMap();
+			String[][] map1 = generateMiscMapOnTurn(turn);
+			String[][] map2 = generateMoverMapOnTurn(turn);
+			String[][] map3 = generatePlayerMap(turn);
 
-		readMoves();
+			board = mergeMaps(map0, map1, map2, map3);	//	error when adding map3 (map3 is empty)
+
+			checkForConflicts();
+			displayBoard();
+
+			turn++;
+		}
 	}
 
-	private static void readBoard() {
+	private static void initialise() {
 		try {
 			Scanner scanFile = new Scanner(new File(filepathBoard));
-
 			boardName = scanFile.nextLine();
 			rows = scanFile.nextInt();
 			columns = scanFile.nextInt();
 			board = new String[rows][columns];
+			totalMoves = getTotalMoves();
+			moves = new String[totalMoves];
 
-			scanFile.nextLine();
-
-			System.out.println(boardName);
-			System.out.printf("rows: %d\tcolumns: %d\n", rows, columns);
-
-			for(int i = 0; i < rows; i++) {
-				String row = scanFile.nextLine();
-				Scanner scanRow = new Scanner(row).useDelimiter("");
-
-				for(int j = 0; j < columns; j++) {
-					board[i][j] = scanRow.next();
+			try {
+				Scanner scanMoves = new Scanner(new File(filepathMoves)).useDelimiter("");
+				int i = 0;
+				while(scanMoves.hasNext()) {
+					moves[i] = scanMoves.next();
+					i++;
 				}
+			}
+			catch(FileNotFoundException fnfex) {
+				fnfex.printStackTrace();
 			}
 		}
 		catch(FileNotFoundException fnfex) {
 			fnfex.printStackTrace();
 		}
-
-		displayBoard();
 	}
 
 	private static int getTotalMoves() {
@@ -119,76 +121,7 @@ public class Traversal {
 		System.out.println();
 	}
 
-	private static void readMoves() {
-		try {
-			Scanner scanFile = new Scanner(new File(filepathMoves));
-
-			if(scanFile.hasNext()) {
-				String line = scanFile.nextLine();
-
-				//	run each turn
-				for(int i = 0; i < line.length(); i++) {
-					char currentMove = line.charAt(i);
-
-					switch(currentMove) {
-						case 'h': move('L'); break;
-						case 'l': move('R'); break;
-						case 'j': move('D'); break;
-						case 'k': move('U'); break;
-						case 'x': quit(); break;
-						default: invalidMove();
-					}
-				}
-			}
-			else {
-				System.out.println("No moves in moves file!");
-			}
-		}
-		catch(FileNotFoundException fnfex) {
-			fnfex.printStackTrace();
-		}
-	}
-
-	private static void refreshPlayerPosition() {
-		int rowNum = -1, columnNum;
-
-		for(String[] row : board) {
-			columnNum = -1;
-			rowNum++;
-
-			for(String piece : row) {
-				columnNum++;
-
-				if (piece.equalsIgnoreCase("s")) {
-					//	set row
-					currentPlayerPosition[ROW] = rowNum;
-					//	set column
-					currentPlayerPosition[COLUMN] = columnNum;
-				}
-			}
-		}
-	}
-
 	private static void wrapDestination(int[] destination, boolean playerPiece) {
-		if(destination[ROW] < 0) {
-			destination[ROW] = board.length - 1;
-		}
-		else if(destination[ROW] > board.length - 1) {
-			destination[ROW] = 0;
-		}
-
-		//	prevents player pieces from being able to wrap around horizontally
-		if(!playerPiece) {
-			if(destination[COLUMN] < 0) {
-				destination[COLUMN] = board[0].length - 1;
-			}
-			else if(destination[COLUMN] > board[0].length - 1) {
-				destination[COLUMN] = 0;
-			}
-		}
-	}
-
-	private static void newWrapDestination(int[] destination, boolean playerPiece) {
 		while(destination[ROW] < 0) {
 			destination[ROW] += rows;
 		}
@@ -206,99 +139,48 @@ public class Traversal {
 		}
 	}
 
-	private static void move(char direction) {
-		turn++;
+	private static void checkForConflicts() {
+		//	will search for overlapping pieces and give suitable responses
+	}
 
-		//	placementQueue item format:		turn;piece;row;column
-		for(String s : placementQueue) {
-			Scanner scanQueueItem = new Scanner(s).useDelimiter(";");
+	//	this method will load the given maps on top of each other in the order given
+	private static String[][] mergeMaps(String[][]... maps) {
+		String[][] mergedMap = new String[rows][columns];
 
-			int turnToPlace = scanQueueItem.nextInt();
-			String piece = scanQueueItem.next();
-			int row = scanQueueItem.nextInt();
-			int column = scanQueueItem.nextInt();
+		for(String[] row : mergedMap) {
+			Arrays.fill(row, "?");
+		}
 
-			//	checking if the queue item is in the turn it is meant to be placed
-			if(turnToPlace == turn) {
+		for(String[][] map : maps) {
+			for(int i = 0; i < rows; i++) {
+				for(int j = 0; j < columns; j++) {
+					String piece = map[i][j];
 
-				System.out.println("next queue item reached");
-
-				board[row][column - 1] = piece;
+					if(!piece.equals("?")) {
+						mergedMap[i][j] = piece;
+					}
+				}
 			}
 		}
 
-		//	first refresh player pos.
-		refreshPlayerPosition();
-
-		//	creating a string to hold the info. for the movers (so they move after the player)
-		String moverInfo = "";
-
-		//	setting destination coordinates
-		int[] destinationPos = new int[2];
-
-		switch(direction) {
-			case 'L':
-				destinationPos[ROW] = currentPlayerPosition[ROW];
-				destinationPos[COLUMN] = currentPlayerPosition[COLUMN] - 1;
-				moverInfo = "h";
-				break;
-			case 'R':
-				destinationPos[ROW] = currentPlayerPosition[ROW];
-				destinationPos[COLUMN] = currentPlayerPosition[COLUMN] + 1;
-				moverInfo = "h";
-				break;
-			case 'D':
-				destinationPos[COLUMN] = currentPlayerPosition[COLUMN];
-				destinationPos[ROW] = currentPlayerPosition[ROW] + 1;
-				moverInfo = "v";
-				break;
-			case 'U':
-				destinationPos[COLUMN] = currentPlayerPosition[COLUMN];
-				destinationPos[ROW] = currentPlayerPosition[ROW] - 1;
-				moverInfo = "v";
-				break;
-		}
-		//	finding the symbol of the destination
-		String destinationSymbol = "";
-
-		try {
-			wrapDestination(destinationPos, true);
-			destinationSymbol = board[destinationPos[ROW]][destinationPos[COLUMN]];
-		}
-		catch(ArrayIndexOutOfBoundsException aioobe) {
-			invalidMove();
-		}
-
-		//	if destination is unoccupied
-		switch(destinationSymbol) {
-			case ".":	//	empty cell
-				swapPiece(currentPlayerPosition, destinationPos);
-				break;
-
-			case "t":	//	target piece
-			case "T":
-				board[destinationPos[ROW]][destinationPos[COLUMN]] = ".";
-				swapPiece(currentPlayerPosition, destinationPos);
-				gameWin();
-				break;
-
-			case "x":	//	wall piece
-			case "X":
-				board[destinationPos[ROW]][destinationPos[COLUMN]] = ".";
-				swapPiece(currentPlayerPosition, destinationPos);
-				gameLose();
-				break;
-		}
-
-		if(!moverInfo.equals("")) {
-			shiftMovers(moverInfo);
-		}
-
-		displayBoard();
+		return mergedMap;
 	}
 
-	private static String[][] generateMiscMap() {
-		String[][] miscMap = new String[board.length][board[0].length];
+	private static String[][] generateBlankMap() {
+		String[][] blankMap = new String[rows][columns];
+
+		for(int row = 0; row < rows; row++) {
+			for(int column = 0; column < columns; column++) {
+				blankMap[row][column] = ".";
+			}
+		}
+
+		return blankMap;
+	}
+
+	private static String[][] generateMiscMapOnTurn(int requestedTurn) {
+		String[][] miscMap = new String[rows][columns];
+
 		try {
 			Scanner scanBoard = new Scanner(new File(filepathBoard));
 
@@ -315,10 +197,12 @@ public class Traversal {
 						miscMap[i][j] = piece;
 					}
 					else {
-						miscMap[i][j] = ".";
+						miscMap[i][j] = "?";
 					}
 				}
+				scanRow.close();
 			}
+			scanBoard.close();
 		}
 		catch(FileNotFoundException fnfex) {
 			fnfex.printStackTrace();
@@ -327,41 +211,19 @@ public class Traversal {
 		return miscMap;
 	}
 
-	private static void generateMoverMap() {
-
-	}
-
-	private static String[][] getMoverMapOnTurn(int requestedTurn) {
+	private static String[][] generateMoverMapOnTurn(int requestedTurn) {
 		String[][] newMoverMap = new String[rows][columns];
 		String[][] mapOnlyMovers = new String[rows][columns];
 
 		for(int i = 0; i < rows; i++) {
 			for(int j = 0; j < columns; j++) {
-				newMoverMap[i][j] = ".";
-				mapOnlyMovers[i][j] = ".";
+				newMoverMap[i][j] = "?";
+				mapOnlyMovers[i][j] = "?";
 			}
 		}
 
-		int verticalMoves = 0, horizontalMoves = 0;
-
-		try {
-			Scanner scanMoves = new Scanner(new File(filepathMoves)).useDelimiter("");
-			for(int i = 0; i < requestedTurn; i++) {
-				char move = scanMoves.next().charAt(0);
-
-				if(move == 'j' || move == 'k') {
-					verticalMoves++;
-				}
-				else if(move == 'h' || move == 'l') {
-					horizontalMoves++;
-				}
-			}
-		}
-		catch(FileNotFoundException fnfex) {
-			fnfex.printStackTrace();
-		}
-
-		System.out.println("vertical moves: " + verticalMoves + "\nhorizontal movers: " + horizontalMoves);
+		int verticalMoves = getMovesAtTurn(requestedTurn, "vertical");
+		int horizontalMoves = getMovesAtTurn(requestedTurn, "horizontal");
 
 		try {
 			Scanner scanBoard = new Scanner(new File(filepathBoard));
@@ -378,14 +240,13 @@ public class Traversal {
 						mapOnlyMovers[row][column] = piece;
 					}
 				}
+				scanRow.close();
 			}
+			scanBoard.close();
 		}
 		catch(FileNotFoundException fnfex) {
 			fnfex.printStackTrace();
 		}
-
-		System.out.println("\nmapOnlyMovers:");
-		print2DArray(mapOnlyMovers);
 
 		/*
 		now that we have the number of moves of either vertical or horizontal (at the requested turn),
@@ -401,19 +262,19 @@ public class Traversal {
 				switch(piece) {
 					case "U":
 						destination[ROW] -= verticalMoves;
-						newWrapDestination(destination, false);
+						wrapDestination(destination, false);
 						break;
 					case "D":
 						destination[ROW] += verticalMoves;
-						newWrapDestination(destination, false);
+						wrapDestination(destination, false);
 						break;
 					case "L":
 						destination[COLUMN] -= horizontalMoves;
-						newWrapDestination(destination, false);
+						wrapDestination(destination, false);
 						break;
 					case "R":
 						destination[COLUMN] += horizontalMoves;
-						newWrapDestination(destination, false);
+						wrapDestination(destination, false);
 						break;
 				}
 				if(Arrays.asList(moverTypes).contains(piece)) {
@@ -422,10 +283,109 @@ public class Traversal {
 			}
 		}
 
-		System.out.println("newMoverMap");
-		print2DArray(newMoverMap);
-
 		return newMoverMap;
+	}
+
+	private static String[][] generatePlayerMap(int requestedTurn) {
+		String[][] playerMap = new String[rows][columns];
+		int[] startLoc = new int[2];
+		String pieceUsed = "!";
+
+		try {
+			Scanner scanBoard = new Scanner(new File(filepathBoard));
+			scanBoard.nextLine();
+			scanBoard.nextLine();
+
+			for(int i = 0 ; i < rows; i++) {
+				Scanner scanRow = new Scanner(scanBoard.nextLine()).useDelimiter("");
+
+				for(int j = 0; j < columns; j++) {
+					String piece = scanRow.next();
+
+					if(piece.equalsIgnoreCase("s")) {
+						pieceUsed = piece;
+						startLoc[0] = i;
+						startLoc[1] = j;
+					}
+				}
+				scanRow.close();
+			}
+			scanBoard.close();
+		}
+		catch(FileNotFoundException fnfex) {
+			fnfex.printStackTrace();
+		}
+
+		//	now find current location
+		int[] currentLoc = startLoc.clone();
+		int counter = 1;
+
+		for(String move : moves) {
+			if(counter <= requestedTurn) {
+				switch(move) {
+					case "h":	//	left
+						currentLoc[COLUMN]--;
+						break;
+					case "l":	//	right
+						currentLoc[COLUMN]++;
+						break;
+					case "j":	//	down
+						currentLoc[ROW]++;
+						break;
+					case "k":	//	up
+						currentLoc[ROW]--;
+						break;
+				}
+			}
+			counter++;
+		}
+
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < columns; j++) {
+				if(i == currentLoc[ROW] && j == currentLoc[COLUMN]) {
+					playerMap[i][j] = pieceUsed;
+				}
+				else {
+					playerMap[i][j] = "?";
+				}
+			}
+		}
+
+		return playerMap;
+	}
+
+	private static int getMovesAtTurn(int requestedTurn, String moveType) {
+		int verticalMoves = 0, horizontalMoves = 0;
+
+		try {
+			Scanner scanMoves = new Scanner(new File(filepathMoves)).useDelimiter("");
+			for(int i = 0; i < requestedTurn; i++) {
+				char move = scanMoves.next().charAt(0);
+
+				if(move == 'j' || move == 'k') {
+					verticalMoves++;
+				}
+				else if(move == 'h' || move == 'l') {
+					horizontalMoves++;
+				}
+			}
+			scanMoves.close();
+		}
+		catch(FileNotFoundException fnfex) {
+			fnfex.printStackTrace();
+		}
+
+		if(moveType.equalsIgnoreCase("horizontal")) {
+			return horizontalMoves;
+		}
+		else if(moveType.equalsIgnoreCase("vertical")) {
+			return verticalMoves;
+		}
+		else {
+			System.out.println("That is not a move type! Exiting...");
+			System.exit(0);
+			return -1;
+		}
 	}
 
 	private static void print2DArray(String[][] array) {
@@ -434,91 +394,6 @@ public class Traversal {
 				System.out.print(piece);
 			}
 			System.out.println();
-		}
-	}
-
-	private static void shiftMovers(String playerDirection) {
-		//	creating an array to hold the pieces that will be moved
-		int[][][] toBeShifted = new int[rows * columns][2][2];
-		int arrCounter = 0;
-		int rowNum = -1;
-
-		for(String[] row : board) {
-			rowNum++;
-			int columnNum = -1;
-
-			for(String piece : row) {
-				columnNum++;
-
-				int[] origin = {rowNum, columnNum};
-				int[] destination = origin.clone();
-
-				//	vertical movers
-				if(playerDirection.equalsIgnoreCase("v")) {
-					switch(piece.toUpperCase()) {
-						case "U":
-							destination[ROW]--;
-							break;
-						case "D":
-							destination[ROW]++;
-							break;
-					}
-				}
-				//	horizontal movers
-				else if(playerDirection.equalsIgnoreCase("h")) {
-					switch(piece.toUpperCase()) {
-						case "L":
-							destination[COLUMN]--;
-							break;
-						case "R":
-							destination[COLUMN]++;
-							break;
-					}
-				}
-
-				if(!Arrays.equals(origin, destination)) {
-					wrapDestination(destination, false);
-
-					toBeShifted[arrCounter][0] = origin;
-					toBeShifted[arrCounter][1] = destination;
-
-					arrCounter++;
-				}
-			}
-		}
-
-		//	bulk swap all movers
-		bulkShift(toBeShifted);
-	}
-
-	private static void bulkShift(int[][][] toBeShifted) {
-		for(int[][] pair : toBeShifted) {
-			swapPiece(pair[0], pair[1]);
-		}
-	}
-
-	private static void swapPiece(int[] origin, int[] destination) {
-		if(!Arrays.equals(origin, destination)) {
-			if(getPieceAt(destination).equals(".")) {
-				String temp = board[destination[ROW]][destination[COLUMN]];
-				board[destination[ROW]][destination[COLUMN]] = board[origin[ROW]][origin[COLUMN]];
-				board[origin[ROW]][origin[COLUMN]] = temp;
-			}
-			else {
-				//	check if piece hit is another mover
-				if(Arrays.asList(moverTypes).contains(getPieceAt(destination))) {
-					System.out.println("hit another mover!");
-
-					//	get direction of piece by subtracting origin from destination
-					int[] direction = new int[origin.length];
-
-					for(int i = 0; i < origin.length; i++) {
-						direction[i] = destination[i] - origin[i];
-					}
-
-					System.out.println(Arrays.toString(direction));
-				}
-			}
 		}
 	}
 
